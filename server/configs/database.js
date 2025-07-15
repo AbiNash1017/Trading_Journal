829b3c6cd243f815e5c1b6b9866f39d07eb013aa
Abinash Das <abinash9731@gmail.com>
2025-07-15 22:26:44 +0530

    backend added

--- server/configs/database.js
+++ server/configs/database.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
     try {
         mongoose.connection.on('connected',()=> console.log("Database Connected"));
-        mongoose.connect(`${process.env.MONGODB_URI}/NexStay`);
+        mongoose.connect(`${process.env.MONGODB_URI}/ForeNotes`);
     } catch (error) {
         console.log(error.message);
     }
