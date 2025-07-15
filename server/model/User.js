14ce1c526356ed7745ff1b17e3a50a111cbfb792
Abinash Das <abinash9731@gmail.com>
2025-07-15 22:57:45 +0530

    user

---
+++ server/model/User.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const userSchema= mongoose.Schema({
+    _id:{type:String, required: true},
+    username:{type:String, required: true},
+    email:{type:String, required: true},
+
+},{timestamps:true});
+
+const User =mongoose.model("User", userSchema);
+
+export default User;
